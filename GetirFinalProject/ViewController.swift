463ec280ae575a1282358569546caca1425ea775
Alphan Ogün <alphanogun@gmail.com>
2024-04-09 18:58:07 +0300

    Initial commit

---
+++ GetirFinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetirFinalProject
+//
+//  Created by Alphan Ogün on 9.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
